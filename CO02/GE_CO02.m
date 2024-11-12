ade185a91f8be6ce2e74eb90e2673d682e81992c
Gareth Everton <gareth.everton@outlook.com>
2024-11-12 00:14:19 +0000

    Started Nuclear Decay

--- CO02/GE_CO02.m
+++ CO02/GE_CO02.m
@@ -102,7 +102,7 @@ end
 function [a, b] = linearCurveFit(x, y)
     N = length(x);
     if length(y) ~= N
-        error("Input arrays of unequal lenght");
+        error("Input arrays of unequal length");
     end
 
     sumX = 0;
@@ -124,6 +124,7 @@ function [a, b] = linearCurveFit(x, y)
     a = vecA(1);
     b = vecA(2);
 end
+
 function r = calculateRMS(y, yFit)
     rSq = 0;
     for i = 1 : length(y)
@@ -152,6 +153,100 @@ function linearFitExecution(fileID, x, y)
 end
 
 
+function [x, y] = polynomialGenerator(N, m, a, e)
+    if m + 1 ~= length(a)
+        %error(["Incorrect number of coefficients: Polynomial of order ",num2str(m)," was specified but ",num2str(length(a))," coefficients were specified"]);
+        error("Incorrect number of coefficients");
+    end
+    x = zeros(1, N+1);
+    y = zeros(1, N+1);
+    for i = 0 : N
+        x(i + 1) = -5  + 10 * i / N;
+        y(i + 1) = 0;
+        for j  = 1 : length(a)
+            y(i + 1) = y(i +1) + a(j) * x(i + 1).^(j-1);
+        end
+        y(i + 1) = y(i + 1) + e * randn();
+    end
+end
+
+function [a, yFit, r] = polynomialFit(x, y, m)
+    %Want to minimise compute time for these matrices, so preallocate arrays
+    n = m + 1; %Polynomial order m has n cofficients
+
+
+    xSums = zeros(1, 2 * n);
+    ySums = zeros(1, n);
+    yFit = zeros(1,length(x));
+    matX = zeros(n, n);
+    for i = 1 : n
+        for j = 1 : length(x)
+            ySums(i) = ySums(i) + y(j) * x(j).^(i-1);
+            xSums(i) = xSums(i) + x(j).^(i-1); %Perform xSum within one loop to reduce loop overhead and make use of AVX (hopefully - not sure if it makes a difference in MATLAB but defo does in C++)
+            xSums(i + n) = xSums(i + n) + x(j).^(i-1+n);
+        end
+    end
+    for i = 1:n
+        for j = 1:n
+            matX(i, j) = xSums(i+j-1);
+        end
+    end
+
+    a = matX\ySums';
+
+    for i = 1 : length(x)
+        for j  = 1 : n
+            yFit(i) = yFit(i) + a(j) * x(i).^(j-1);
+        end
+    end
+    plot(x, y, 'b.');
+    hold on
+    plot(x, yFit, 'r-')
+
+    r = calculateRMS(y, yFit);
+    
+
+
+end
+
+function polynomialFitExecute(fileID)
+    aReal = [1.5, -2.5, 0.7, -1.2];
+    [x, y] = polynomialGenerator(100, 3, aReal, 1);
+    [aFit, yFit, r] = polynomialFit(x, y, 3);
+    fprintf(fileID, '%s', 'X:,Y:,Y_FIT,y = ');
+    for i = 1 : length(aFit)
+        fprintf(fileID, '%s', [num2str(aFit(i), 3), +'x^', num2str(i-1, 1), ' ']);
+    end
+    fprintf(fileID, '%s', [',r = ',num2str(r, 5), newline]);
+    for i = 1 : length(x)
+        fprintf(fileID, '%s', [num2str(x(i), 3),', ',num2str(y(i), 3),', ',num2str(yFit(i), 3),newline]);
+    end
+
+end
+function [x, y, z] = evaluateDecayEvent(t, aX, aY, x0)
+    x = x0 * exp(-aX * t);
+    if aX == aY
+        y = x0 * aY * t * exp(-aY * t);
+    else
+        y = x0 * aX * ((exp(-aX * t) - exp(-aY * t)) / (aY - aX));
+    end
+    z = x0 - x- y;
+end
+
+function [t, x, y, z]  = executeNuclearDecay(tF, dt, tX, tY, x0)
+    aX = log(2)/tX;
+    aY = log(2)/tY;
+    count = ceil(tF/dt) + 1;
+    t = zeros(1,count);
+    x = zeros(1,count);
+    y = zeros(1,count);
+    z = zeros(1,count);
+    x(1) = x0;
+    for i = 1 : count
+        [x(i+1), y(i+1), z(i+1)] = evaluateDecayEvent(i * dt, aX, aY, x0);
+        t(i+1) = i * dt;
+    end
+end
 
 
 
@@ -163,8 +258,8 @@ end
 
 
 fileID = fopen('GE_CO02_Output.csv', 'w'); %open File for data output
-%EXERCISE_NUMBER = input("Select Exercise: (1 - 3): ");
-EXERCISE_NUMBER = 3;
+%EXERCISE_NUMBER = input("Select Exercise: (1 - 4): ");
+EXERCISE_NUMBER = 4;
 %Main code
 switch EXERCISE_NUMBER
     case 1
@@ -219,7 +314,7 @@ switch EXERCISE_NUMBER
     case 3
         %Exercise 3: Curve Fitting
         %CALC_ORDER = input("Order of the best fit line to be calculated: 1 = Linear, 2 = Quadratic etc: ");
-        CALC_ORDER = 1;
+        CALC_ORDER = 2;
         switch CALC_ORDER
             case 1
                 data = load("linear.csv");
@@ -228,9 +323,30 @@ switch EXERCISE_NUMBER
                 y = data(:, 2);
 
                 linearFitExecution(fileID, x, y);
-                
+            case 2
+                polynomialFitExecute(fileID);
+
 
         end
+
+    case 4
+        %HALF_LIFE_X = input("Half life of element X (Years): ");
+        %HALF_LIFE_Y = input("Half life of element Y (Years): ");
+
+        HALF_LIFE_X = 2;
+        HALF_LIFE_Y = 16;
+
+        [t, x, y, z] = executeNuclearDecay(50, 1, HALF_LIFE_X, HALF_LIFE_Y, 1000);
+        fprintf(fileID, '%s', ["t, x, y, z",newline]);
+        for i = 1 : length(t)
+            fprintf(fileID, '%s', [num2str(t(i)), ', ', num2str(x(i), 3), ', ', num2str(y(i), 3), ', ', num2str(z(i), 3), newline]);
+        end
+
+        plot(t, x, 'b-');
+        hold on;
+        plot(t, y, 'r-');
+        plot(t, z, 'g-');
+
 
 end
 fclose(fileID);
---
+++ EM01/EM01_DA/EM01_DA.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+from scipy.optimize import curve_fit
+
+
+thicknessRAW = np.array([2.94,4.84,7.78,12.41,15.35,17.25,20.19,2.94,4.84,7.78,12.41,15.35,17.25,20.19,2.94,4.84,7.78,12.41,15.35,17.25,20.19])
+
+print(thicknessRAW[2])
